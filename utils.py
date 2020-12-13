bdd336f06ad8cc99180c32df966aa114449a3a4a
Javier Rando Ramírez <javier.rando@tum.de>
2020-12-13 13:31:48 +0100

    Updates

--- utils.py
+++ utils.py
@@ -1,10 +1,85 @@
+import nltk
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
+import re
 from sklearn.manifold import TSNE
 import scipy.cluster.hierarchy as sch
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 import matplotlib.pyplot as plt
 import pandas as pd
-import nltk
+
+
+def clean_text(input_text: str,
+               STOPWORDS=stopwords.words('english')):
+    """
+    Given an input sentence, it returns a processed string after applying:
+    - Lowercasing
+    - Multiple spaces removal
+    - Removing mention to users (@XXXX)
+    - Removing punctuation and other undesired symbols (# will be kept since they are essential to understand tweets)
+    - Stop-words removal
+    - Stemming (Using Porter Stemmer) - HASHTAGS will not be stemmed
+    """
+
+    stemmer = PorterStemmer()
+
+    # remove "RT" string indicating a retweet
+    output = input_text.replace("RT", "")
+
+    # remove URLs
+    output = re.sub(r'https?:\/\/.*[\r\n]*', '', output, flags=re.MULTILINE)
+
+    # remove mentions and users in the string
+    output = re.sub(r"\@[\w]+", "", output)
+
+    # Removing multiple spaces
+    output = re.sub(r'\s+', ' ', output)
+
+    # lowering text
+    output = output.lower()
+
+    # removing all the punctuations
+    output = re.sub("[^A-Za-z0-9\s#]+", "", output).strip()
+
+    # remove stopwords
+    tokens = [word for word in output.split() if word not in STOPWORDS]
+
+    # remove double hashtags
+    tokens = [re.sub("[#]+", "#", token) for token in tokens]
+
+    # Stemming. Avoid stemming hashtags
+    output = " ".join([stemmer.stem(token) if not token.startswith('#') else token for token in tokens])
+
+    # remove single letters (meaning errors while writing)
+    output = re.sub(r'\s[A-Z,a-z]\s', '', output)
+
+    return output
+
+def get_tweet_fields(df_tweets,i):
+    """
+        df_tweets: database with the tweets information
+        i: id of the tweet we want to extract the information
+        returns various fields needed for showing the result to the user
+    """
+    user_name = eval(df_tweets.iloc[i]['user'])['name']
+    text = df_tweets.iloc[i]['full_text']
+    entities = eval(df_tweets.iloc[i]['entities'])
+    urls = entities['urls']
+    if urls:
+        url = urls[0]['url']
+        text = text.replace(url,'')
+    else:
+        url = None
+    hashtags = entities['hashtags']
+    if not hashtags:
+        hashtags.append('No hashtags')
+
+    favorite_count = df_tweets.iloc[i]['favorite_count']
+    retweet_count =  df_tweets.iloc[i]['retweet_count']
+
+    return user_name, text, url, hashtags, favorite_count, retweet_count
+
 
 def run_TSNE(X,df_tweets = None,labels = None, doc = False,name = 'default.png'):
     """
@@ -121,3 +196,7 @@ def standardize_text(text,language):
     
     # Return
     return standardized_text
+
+
+def personalized_tokenizer(text):
+    return text.split()
